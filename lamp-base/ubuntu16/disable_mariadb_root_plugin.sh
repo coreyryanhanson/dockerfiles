6f81ca8d13c065e2313c13b0df21c07fec743c01
Corey <corey@localhost.localdomain>
2018-01-05 00:06:47 -0500

    added partially finished creepMiner for burst

--- lamp-base/ubuntu16/disable_mariadb_root_plugin.sh
+++ lamp-base/ubuntu16/disable_mariadb_root_plugin.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 #Script to disable MariaDB Root Plugin.
-service mysql start && 
+service mysql start 
 mysql -u root mysql <<EOF
 update user set plugin='' where User='root';
 flush privileges;

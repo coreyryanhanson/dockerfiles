71525538ec8cce1d9b84766626bd02faf5a4cb9f
Corey <corey@localhost.localdomain>
2018-01-06 22:55:04 -0500

    merge fixes

--- lamp-base/ubuntu16/disable_mariadb_root_plugin.sh
+++ lamp-base/ubuntu16/disable_mariadb_root_plugin.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
 
-#Script to disable MariaDB Root Plugin.
-service mysql start 
-mysql -u root mysql <<EOF
-update user set plugin='' where User='root';
-flush privileges;
-commit;
+#Script to disable MariaDB Unix Socket.
+service mysql start
+mysql --user=root --host=localhost mysql<<EOF
+UPDATE mysql.user SET plugin = 'mysql_native_password' WHERE user = 'root' AND plugin = 'unix_socket';
+FLUSH PRIVILEGES;
 EOF

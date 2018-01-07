f453c84530284cbbaa3a794d2fb2dcc566771dab
Corey <corey@localhost.localdomain>
2018-01-07 07:01:17 -0500

    updated ccminer and simplified lamp image

--- lamp-base/ubuntu16/disable_mariadb_root_plugin.sh
+++ lamp-base/ubuntu16/disable_mariadb_root_plugin.sh
@@ -4,5 +4,8 @@
 service mysql start
 mysql --user=root --host=localhost mysql<<EOF
 UPDATE mysql.user SET plugin = 'mysql_native_password' WHERE user = 'root' AND plugin = 'unix_socket';
+SET PASSWORD = password('wizardhorse');
 FLUSH PRIVILEGES;
+quit
 EOF
+service mysql stop

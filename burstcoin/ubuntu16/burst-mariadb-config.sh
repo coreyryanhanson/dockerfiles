dcadd10302ab8e84aa3c9745d4a040a395ec18ef
Corey <corey@localhost.localdomain>
2018-07-07 10:26:10 -0400

    fix authentication errors

--- burstcoin/ubuntu16/burst-mariadb-config.sh
+++ burstcoin/ubuntu16/burst-mariadb-config.sh
@@ -3,7 +3,7 @@
 #Setup databases for Burst Wallet.
 service mysql start
 mysql -u root --password='' -h localhost <<EOF
-  CREATE DATABASE burstwallet;
-  CREATE USER 'burstwallet'@'localhost' IDENTIFIED BY '$docker_unprivileged_pass';
-  GRANT ALL PRIVILEGES ON burstwallet.* TO 'burstwallet'@'localhost';
+  CREATE DATABASE brs_master;
+  CREATE USER 'brs_user'@'localhost' IDENTIFIED BY '$docker_unprivileged_pass';
+  GRANT ALL PRIVILEGES ON brs_master.* TO 'brs_user'@'localhost';
 EOF
